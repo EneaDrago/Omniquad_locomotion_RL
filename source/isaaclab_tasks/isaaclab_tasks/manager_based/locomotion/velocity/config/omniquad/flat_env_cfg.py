e7df535cbc37f075c4ab494bf9ca778ed4168792
EneaDrago <enea.dragoni@gmail.com>
2025-06-21 19:27:09 +0200

    create flat environment

--- source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/config/omniquad/flat_env_cfg.py
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/config/omniquad/flat_env_cfg.py
@@ -12,13 +12,9 @@ class OmniQuadFlatEnvCfg(OmniQuadRoughEnvCfg):
         # override rewards
         self.rewards.flat_orientation_l2.weight = -5.0
         self.rewards.dof_torques_l2.weight = -2.5e-5
-        self.rewards.feet_air_time.weight = 0.5
         # change terrain to flat
         self.scene.terrain.terrain_type = "plane"
         self.scene.terrain.terrain_generator = None
-        # no height scan
-        self.scene.height_scanner = None
-        self.observations.policy.height_scan = None
         # no terrain curriculum
         self.curriculum.terrain_levels = None
 
