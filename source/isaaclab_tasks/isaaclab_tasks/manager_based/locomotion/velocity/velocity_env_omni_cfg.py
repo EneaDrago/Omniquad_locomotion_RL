d70d056d7774942fef015f14e6a91193eafc1ded
EneaDrago <enea.dragoni@gmail.com>
2025-06-22 16:45:39 +0200

    add feet air time

--- source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/velocity_env_omni_cfg.py
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/velocity_env_omni_cfg.py
@@ -257,15 +257,15 @@ class RewardsCfg:
         },
     )
     # joint_deviation_l1 = RewTerm(func=mdp.joint_deviation_l1, weight=-0.01)
-    # feet_air_time = RewTerm(
-    #     func=mdp.feet_air_time,
-    #     weight=-5.0,
-    #     params={
-    #         "sensor_cfg": SceneEntityCfg("contact_forces", body_names=".*LOWER_LEG"),
-    #         "command_name": "base_velocity",
-    #         "threshold": 0.01,
-    #     },
-    # )
+    feet_air_time = RewTerm(
+        func=mdp.feet_air_time,
+        weight=-5.0,
+        params={
+            "sensor_cfg": SceneEntityCfg("contact_forces", body_names=".*LOWER_LEG"),
+            "command_name": "base_velocity",
+            "threshold": 0.01,
+        },
+    )
 
     undesired_contacts = RewTerm(
         func=mdp.undesired_contacts,
